d913ce3178726724e6c32ec8e8810be2931332f0
Chris-Bortel <bortelcc19@gmail.com>
2020-11-19 14:45:44 -0800

    Form.js is complete with the difficulty range added and being included in the state being sent back to app

--- src/components/todo/form.js
+++ src/components/todo/form.js
@@ -5,7 +5,7 @@ export default function UserForm(props) {
 
   const [details, setDetails] = useState('');
   const [assignee, setAssignee] = useState('');
-  // const [difficulty, setDifficulty] = useState('');
+  const [difficulty, setDifficulty] = useState('');
   
   function handleChangeDetails(event) {
     const newDetails = event.target.value
@@ -18,12 +18,19 @@ export default function UserForm(props) {
     setAssignee(newAssignee)
   }
 
+  function handleChangeDifficulty (event) {
+    const newDifficulty = event.target.value
+    console.log(newDifficulty)
+    setDifficulty(newDifficulty)
+  }
+
   function handleSubmit(e) {
     e.preventDefault()
     e.target.reset()
-    props.handler({details, assignee})
+    props.handler({details, assignee, difficulty})
     setDetails('')
     setAssignee('')
+    setDifficulty('')
   }
 
   return (
@@ -32,13 +39,37 @@ export default function UserForm(props) {
     
         <Form.Group controlId="formGroupDetails">
           <Form.Label>To Do Item</Form.Label>
-          <Form.Control type="input" value={details} onChange= {handleChangeDetails} placeholder="Item Details" />
+          <Form.Control 
+                type="input" 
+                value={details} 
+                placeholder="Item Details" 
+                onChange= {handleChangeDetails} 
+                />
 
         </Form.Group>
         <Form.Group controlId="formGroupAssignee">
           <Form.Label>Assigned to</Form.Label>
-          <Form.Control type="input" value={assignee} onChange= {handleChangeAssignee} placeholder="Assignee Name" />
+          <Form.Control 
+                type="input" 
+                value={assignee} 
+                placeholder="Assignee Name" 
+                onChange= {handleChangeAssignee} 
+                />
         </Form.Group>
+
+        <Form.Group controlId="formBasicRangeCustom">
+          <Form.Label>Range</Form.Label>
+          <Form.Control 
+                defaultValue="1"
+                type="range"
+                min="1"
+                max="5"
+                name="text"
+                placeholder="Assignee Name"
+                onChange={handleChangeDifficulty}
+                />
+        </Form.Group>
+        
         <Button variant="primary" type="submit">Submit</Button>
       </Form>
     </>
