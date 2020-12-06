80822c33e2fc483f3091fec5ba028413566c53ca
Chris-Bortel <bortelcc19@gmail.com>
2020-12-06 15:36:42 -0800

    I added pagination that does not quiet woork, so I commented it out. I also found a bug that logs the user out when refreshing the page

--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -56,3 +56,45 @@ return(
   </SettingsContext.Consumer>
   )
 }
+
+
+// return(
+
+//   <SettingsContext.Consumer>
+//   { settingsContext => {
+//         const switchTasks = settingsContext.settings.showComplete === false ? filteredCompletedTasks : props.list;
+
+//     return(
+//         <PaginatedList
+//         list = {props.list}
+//         itemsPerPage = {3}
+//         renderList = {list => (
+//         <>
+//           { switchTasks.map(item => (
+
+//             <Toast key={item._id} onClose={() => props.handleDelete(item._id)}>
+//               <Toast.Header closeButton={canDo(loginContext.users)}>
+//                 <div onClick={() => props.handleComplete(item._id)}>
+//                     <Badge  pill variant="danger" className={`${item.complete? 'bg-danger':'bg-success'}`} key={item._id}>
+//                       {item.complete? 'Complete':'Pending'}
+//                     </Badge>
+//                 </div>
+//                 <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
+//                 <strong className="mr-auto">{item.assignee}</strong>
+//               </Toast.Header>
+
+//               <Toast.Body>{item.text}
+//                 <span className="d-flex justify-content-end">
+//                   <small>Difficulty: {item.difficulty} </small>
+//                 </span>
+//               </Toast.Body>
+//             </Toast>
+
+//           ))}
+//         </>
+//       )}
+  
+//       />
+//     )}}
+//     </SettingsContext.Consumer>
+// )
