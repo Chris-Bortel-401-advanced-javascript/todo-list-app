656b207ab874b6dbcbc22edd4e34fe69b73de662
Chris-Bortel <bortelcc19@gmail.com>
2020-12-05 18:59:02 -0800

    Bug fix

--- src/context/auth/context.js
+++ src/context/auth/context.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import superagent from 'superagent';
 import jwt from 'jsonwebtoken';
 import cookie from 'react-cookies';
